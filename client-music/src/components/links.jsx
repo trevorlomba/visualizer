dd356ea1c3de180ff556de50603bec1fb7b7c94f
Trevor <trevor@Trevors-MacBook-Pro.local>
2022-08-05 15:14:17 -0400

    favicon

--- client-music/src/components/links.jsx
+++ client-music/src/components/links.jsx
@@ -2,27 +2,33 @@ import React from 'react'
 import { TiSocialInstagram } from 'react-icons/ti'
 import { TbBrandSpotify } from 'react-icons/tb'
 import { AiOutlineApple } from 'react-icons/ai'
-import { IoShirtOutline } from 'react-icons/io5'
+import { IoShirtOutline, IoLogoTiktok } from 'react-icons/io5'
 import { ImSpotify } from 'react-icons/im'
 import { RiSpotifyLine } from 'react-icons/ri'
 
 const links = [
 	{
+		name: 'TikTok',
+		tag: 'tiktok',
+		link: 'https://www.tiktok.com/@lxvimusic',
+		img: <IoLogoTiktok />,
+	},
+	{
 		name: 'Instagram',
 		tag: 'instagram',
-		link: 'https://www.instagram.com/',
+		link: 'https://www.instagram.com/lxvimusic',
 		img: <TiSocialInstagram />,
 	},
 	{
 		name: 'Spotify',
 		tag: 'spotify',
-		link: 'https://www.instagram.com/',
+		link: 'https://open.spotify.com/artist/1VvdebwNf9wAPhoti1uQtn?C=',
 		img: <RiSpotifyLine />,
 	},
 	{
 		name: 'Apple Music',
 		tag: 'apple',
-		link: 'https://www.instagram.com/',
+		link: 'https://music.apple.com/us/artist/lxvi/990781367',
 		img: <AiOutlineApple />,
 	},
 	{
