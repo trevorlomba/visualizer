7dc3c99b1cf86622cf9a4e4cc70a4678a7f74a4f
Trevor <trevor@Trevors-MacBook-Pro.local>
2022-08-06 17:12:45 -0400

    playback via howlerReact

--- client-music/src/App.js
+++ client-music/src/App.js
@@ -3,15 +3,46 @@ import './App.css';
 import Featured from './components/Featured';
 import { useEffect, useState } from 'react';
 
+// import { Howl, Howler } from 'howler'
+import ReactHowler from 'react-howler'
+
+import { AiFillPauseCircle, AiFillPlayCircle } from 'react-icons/ai'
 
 function App() {
   const [artistName, setArtistName] = useState('yes')
+  const audio = './assets/sound.wav'
+  const [playing, setPlaying] = useState(false)
+//   let sound = new Howl({
+// 	  src: audio,
+// 	  autoplay: true
+// 	})
+// 	console.log(sound)
+	
+// 	const playSound = function() {
+// 	  console.log('soundplay')
+// 	  console.log(sound)``
+// 	  sound.play()
+// 	}
+	
 
-  useEffect(() => 
-    console.log(artistName)
-  , [])
+  useEffect(() => {}, [])
+  
+
   return (
 		<div className='App'>
+			<div className='playButton' onClick={() => setPlaying(!playing)}>
+				<div className={playing ? 'pause' : 'play'}>
+					{!playing ? <AiFillPlayCircle /> : <AiFillPauseCircle />}
+				</div>
+			</div>
+			<ReactHowler
+				src='https://sndup.net/ds68/d'
+				playing={playing}
+				html5={true}
+				preload={true}
+				format={'m4a'}
+				loop={true}
+			/>
 			<Featured
 				artistName={artistName}
 				setArtistName={setArtistName}
