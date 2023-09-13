b3610058e40faab97d964a5a7c08855e8ea9199f
trevorlomba <trevorlomba@gmail.com>
2023-09-13 07:41:04 -0400

    trying to set up audiocontext, might have to ditch Howler

--- client-music/src/App.js
+++ client-music/src/App.js
@@ -3,8 +3,13 @@ import './App.scss';
 import songs from './songs'
 import backgrounds from './backgrounds';
 import Featured from './components/Featured';
-import { useEffect, useState } from 'react';
-import { BrowserRouter, NavLink } from 'react-router-dom'
+import { useEffect, useState, useRef } from 'react';
+import { BrowserRouter, NavLink } from 'react-router-dom';
+import { Howl, Howler } from 'howler';
+import useTunaEffect from './components/useTunaEffect';
+import tuna from 'tunajs';
+
+
 
 // import { Howl, Howler } from 'howler'
 import ReactHowler from 'react-howler'
@@ -12,6 +17,10 @@ import ReactHowler from 'react-howler'
 import ReactGA from 'react-ga'
 ReactGA.initialize('G-22X1L2K6WV')
 ReactGA.pageview(window.location.pathname + window.location.search)
+
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+var tunaObj = new tuna(audioCtx);
 
 
 let songsList = []
@@ -38,19 +47,121 @@ function App() {
   useEffect(() => {}, [])
   console.log(songs)
 
+	const sourceRef = useRef(null); // Ref to keep track of the audio source node
+
+	async function loadAudioFile(url) {
+		const response = await fetch(url);
+		const arrayBuffer = await response.arrayBuffer();
+		const audioBuffer = await audioCtx.decodeAudioData(arrayBuffer);
+		return audioBuffer;
+	}
+
+	function playAudio() {
+		if (sourceRef.current) {
+			sourceRef.current.stop();
+		}
+
+		loadAudioFile(songsList[song].songLink).then((audioBuffer) => {
+			const source = audioCtx.createBufferSource();
+			source.buffer = audioBuffer;
+			sourceRef.current = source;
+
+			const gain = new tunaObj.Gain({
+				gain: vocalVolume
+			});
+
+			source.connect(gain);
+			gain.connect(audioCtx.destination);
+
+			source.start();
+		});
+	}
+
+	useEffect(() => {
+		if (playing) {
+			playAudio();
+		} else if (sourceRef.current) {
+			sourceRef.current.stop();
+			sourceRef.current = null;
+		}
+	}, [playing, song]);
+
+
+	function handleUserInteraction() {
+		if (audioCtx.state === 'suspended') {
+			audioCtx.resume();
+		}
+
+		// // Initialize Howler with the AudioContext
+		// AudioContext()
+
+		// ... other logic
+	}
+
+
+  const howlRef = useRef(null);
+
+
+	function applyTunaEffect(howlerSound) {
+
+		if (howlerSound) {
+			const gain = new tunaObj.Gain({
+				gain: .1
+			});
+
+			console.log(howlRef.current.howler);
+			console.log(typeof howlRef.current.howler._sounds[0]._node);
+			console.log(howlRef.current.howler._sounds[0]._node);
+
+			console.log(howlerSound);
+
+			console.log(`howlerSound', ${howlerSound}`)
+
+			// Disconnect Howler's masterGain from destination and connect it to gain
+			console.log(howlerSound._sounds[0]._node)
+			console.log(howlerSound)
+			howlerSound._sounds[0]._node.disconnect();
+			howlerSound._sounds[0]._node.connect(gain);
+
+			// Connect gain to destination
+			gain.connect(audioCtx.destination);
+		}
+	}
+
+	useEffect(() => {
+		if (howlRef.current) {
+			const howlerSound = howlRef.current.howler;
+
+			// Define the function we want to execute once the sound is loaded
+			const onSoundLoad = () => {
+				console.log("Sound loaded!");
+				applyTunaEffect(howlerSound);
+			};
+
+			// Attach the load event listener
+			howlerSound.on('load', onSoundLoad);
+
+			// Clean up the event listener on component unmount or if howlRef changes
+			return () => {
+				howlerSound.off('load', onSoundLoad);
+			};
+		}
+	}, [howlRef]);
+
 
   return (
 		<BrowserRouter basename='/visualizer'>
 			<div className='App'>
-				<ReactHowler
+				{/* <ReactHowler
 					src={songsList[song].songLink}
 					playing={playing}
-					html5={true}
+					html5={false}
 					preload={true}
-				  format={['m4a']}
+				  	format={['m4a']}
 					loop={true}
 					volume={vocalVolume}
-				/>
+				  	ref={howlRef}
+				/> */}
 				{/* <ReactHowler
 					src={songsList[song]}
 					playing={playing}
@@ -92,6 +203,9 @@ function App() {
 					</span>
 				</label>
 			</div> */}
+			  <button onClick={() => setPlaying((prev) => !prev)}>
+				  Toggle Audio
+			  </button>
 			</div>
 		</BrowserRouter>
 	)
